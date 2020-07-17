323c8472e5b38cac7b7ca5e8a5078cf96abcbbf2
Md Shakil Hossain <40922031+mdShakilHossainNsu2018@users.noreply.github.com>
2020-07-17 13:03:12 +0600

    tweet, like, retweet added to the models.py

--- tweets/models.py
+++ tweets/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tweet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tweet
+
+
+class Like(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    likes = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.owner.username
+
+
+class ReTweet(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    base_tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    tweet = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tweet
